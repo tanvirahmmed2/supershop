a5291381062b24cb918af2e36a2260b1a006a3f8
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-02-27 18:54:12 +0600

    fix

--- src/app/(panel)/panel/layout.jsx
+++ src/app/(panel)/panel/layout.jsx
@@ -2,19 +2,18 @@ import PanelNavbar from '@/components/bar/PanelNavbar'
 import PanelSidebar from '@/components/bar/PanelSidebar'
 import React from 'react'
 
-export const metadata={
-    title:'Panel | Super Shop',
-    description:'Panel Site of Super Shop'
+export const metadata = {
+    title: 'Panel | Super Shop',
+    description: 'Panel Site of Super Shop'
 }
 
 const PanelMainLayout = ({ children }) => {
+    
     return (
         <div className='w-full overflow-x-hidden relative pt-14'>
             <PanelNavbar />
-            <div className='w-full flex flex-row'>
-                <PanelSidebar />
-                {children}
-            </div>
+            <PanelSidebar />
+            {children}
         </div>
     )
 }
--- src/components/bar/PanelSidebar.jsx
+++ src/components/bar/PanelSidebar.jsx
@@ -2,6 +2,11 @@
 import React, { useContext, useState } from 'react'
 import { Context } from '../helper/Context'
 import Link from 'next/link'
+import { MdHome, MdInventory, MdSuperscript } from 'react-icons/md'
+import { IoBag, IoCartOutline } from 'react-icons/io5'
+import { RiAlignItemLeftLine, RiCustomerServiceLine, RiCustomSize } from 'react-icons/ri'
+import { TbBrandBandcamp, TbCategory, TbGitBranch } from "react-icons/tb";
+import { CircleUser } from 'lucide-react'
 
 const PanelSidebar = () => {
   const { panelSidebar } = useContext(Context)
@@ -9,93 +14,93 @@ const PanelSidebar = () => {
 
   
   return (
-    <div className={`w-50 bg-white shadow-xl text-lg h-screen fixed z-50 top-14 left-0 flex flex-col gap-1 p-2 ${panelSidebar?'translate-x-0':'-translate-x-full'} ease-in-out transform duration-500`}>
-      <Link href={'/panel'} onClick={() => setOption('')}>Home</Link>
-      <div className='w-full flex flex-col gap-1'>
-        <button onClick={() => setOption('sales')} className='cursor-pointer w-full text-left'>Sales</button>
-        {option === 'sales' && <div className='w-full border-l-2 flex flex-col gap-1 px-2'>
-          <Link href={'/panel/pos'}>POS</Link>
-          <Link href={'/panel/sales-list'}>Sales List</Link>
-          <Link href={'/panel/sales-payments'}>Payments</Link>
+    <div className={`w-50 bg-white shadow-xl text-lg h-screen fixed z-50 top-14 left-0 flex flex-col gap-4 p-2 ${panelSidebar?'translate-x-0':'-translate-x-full'} ease-in-out transform duration-500`}>
+      <Link className='w-full hover:text-white hover:bg-orange-300 px-2 flex flex-row items-center gap-4' href={'/panel'} onClick={() => setOption('')}><MdHome/> Home</Link>
+      <div className='w-full flex flex-col gap-2 px-2'>
+        <button onClick={() => setOption('sales')} className='cursor-pointer w-full text-left hover:bg-orange-300 hover:text-white flex flex-row items-center gap-4'> <IoCartOutline/> Sales</button>
+        {option === 'sales' && <div className='w-full border-l-2 flex flex-col gap-2 px-2'>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/pos'}>POS</Link>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/sales-list'}>Sales List</Link>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/sales-payments'}>Payments</Link>
         </div>
         }
       </div>
 
-      <div className='w-full flex flex-col gap-1'>
-        <button onClick={() => setOption('purchase')} className='cursor-pointer w-full text-left'>Purchase</button>
-        {option === 'purchase' && <div className='w-full border-l-2 flex flex-col gap-1 px-2'>
-          <Link href={'/panel/purchase'}>Purchase</Link>
-          <Link href={'/panel/purchase-list'}>Purchase List</Link>
-          <Link href={'/panel/purchase-payments'}>Payments</Link>
+      <div className='w-full flex flex-col gap-2 px-2'>
+        <button onClick={() => setOption('purchase')} className='cursor-pointer w-full text-left hover:bg-orange-300 hover:text-white flex flex-row items-center gap-4'><IoBag/> Purchase</button>
+        {option === 'purchase' && <div className='w-full border-l-2 flex flex-col gap-2 px-2'>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/purchase'}>Purchase</Link>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/purchase-list'}>Purchase List</Link>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/purchase-payments'}>Payments</Link>
         </div>
         }
       </div>
 
-      <div className='w-full flex flex-col gap-1'>
-        <button onClick={() => setOption('products')} className='cursor-pointer w-full text-left'>Products</button>
-        {option === 'products' && <div className='w-full border-l-2 flex flex-col gap-1 px-2'>
-          <Link href={'/panel/new-product'}>New Products</Link>
-          <Link href={'/panel/product-list'}>Product List</Link>
+      <div className='w-full flex flex-col gap-2 px-2'>
+        <button onClick={() => setOption('products')} className='cursor-pointer w-full text-left hover:bg-orange-300 hover:text-white flex flex-row items-center gap-4'><RiAlignItemLeftLine/> Products</button>
+        {option === 'products' && <div className='w-full border-l-2 flex flex-col gap-2 px-2'>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/new-product'}>New Products</Link>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/product-list'}>Product List</Link>
         </div>
         }
       </div>
 
-      <div className='w-full flex flex-col gap-1'>
-        <button onClick={() => setOption('brands')} className='cursor-pointer w-full text-left'>Brands</button>
-        {option === 'brands' && <div className='w-full border-l-2 flex flex-col gap-1 px-2'>
-          <Link href={'/panel/new-brand'}>New Brand</Link>
-          <Link href={'/panel/brand-list'}>Brand List</Link>
+      <div className='w-full flex flex-col gap-2 px-2'>
+        <button onClick={() => setOption('brands')} className='cursor-pointer w-full text-left hover:bg-orange-300 hover:text-white flex flex-row items-center gap-4'> <TbBrandBandcamp/> Brands</button>
+        {option === 'brands' && <div className='w-full border-l-2 flex flex-col gap-2 px-2'>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/new-brand'}>New Brand</Link>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/brand-list'}>Brand List</Link>
         </div>
         }
       </div>
 
-      <div className='w-full flex flex-col gap-1'>
-        <button onClick={() => setOption('category')} className='cursor-pointer w-full text-left'>Category</button>
-        {option === 'category' && <div className='w-full border-l-2 flex flex-col gap-1 px-2'>
-          <Link href={'/panel/new-category'}>New Category</Link>
-          <Link href={'/panel/category-list'}>Category List</Link>
+      <div className='w-full flex flex-col gap-2 px-2'>
+        <button onClick={() => setOption('category')} className='cursor-pointer w-full text-left hover:bg-orange-300 hover:text-white flex flex-row items-center gap-4'><TbCategory/> Category</button>
+        {option === 'category' && <div className='w-full border-l-2 flex flex-col gap-2 px-2'>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/new-category'}>New Category</Link>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/category-list'}>Category List</Link>
         </div>
         }
       </div>
 
-      <div className='w-full flex flex-col gap-1'>
-        <button onClick={() => setOption('supplier')} className='cursor-pointer w-full text-left'>Supplier</button>
-        {option === 'supplier' && <div className='w-full border-l-2 flex flex-col gap-1 px-2'>
-          <Link href={'/panel/new-supplier'}>New Supplier</Link>
-          <Link href={'/panel/supplier-list'}>Supplier List</Link>
+      <div className='w-full flex flex-col gap-2 px-2'>
+        <button onClick={() => setOption('supplier')} className='cursor-pointer w-full text-left hover:bg-orange-300 hover:text-white flex flex-row items-center gap-4'><MdSuperscript/> Supplier</button>
+        {option === 'supplier' && <div className='w-full border-l-2 flex flex-col gap-2 px-2'>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/new-supplier'}>New Supplier</Link>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/supplier-list'}>Supplier List</Link>
         </div>
         }
       </div>
 
-      <div className='w-full flex flex-col gap-1'>
-        <button onClick={() => setOption('branch')} className='cursor-pointer w-full text-left'>Branch</button>
-        {option === 'branch' && <div className='w-full border-l-2 flex flex-col gap-1 px-2'>
-          <Link href={'/panel/new-branch'}>New Branch</Link>
-          <Link href={'/panel/branch-list'}>Branch List</Link>
+      <div className='w-full flex flex-col gap-2 px-2'>
+        <button onClick={() => setOption('branch')} className='cursor-pointer w-full text-left hover:bg-orange-300 hover:text-white flex flex-row items-center gap-4'><TbGitBranch/> Branch</button>
+        {option === 'branch' && <div className='w-full border-l-2 flex flex-col gap-2 px-2'>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/new-branch'}>New Branch</Link>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/branch-list'}>Branch List</Link>
         </div>
         }
       </div>
 
-      <div className='w-full flex flex-col gap-1'>
-        <button onClick={() => setOption('staff')} className='cursor-pointer w-full text-left'>Staff</button>
-        {option === 'staff' && <div className='w-full border-l-2 flex flex-col gap-1 px-2'>
-          <Link href={'/panel/new-staff'}>New Staff</Link>
-          <Link href={'/panel/staff-list'}>Staff List</Link>
+      <div className='w-full flex flex-col gap-2 px-2'>
+        <button onClick={() => setOption('staff')} className='cursor-pointer w-full text-left hover:bg-orange-300 hover:text-white flex flex-row items-center gap-4'><CircleUser/> Staff</button>
+        {option === 'staff' && <div className='w-full border-l-2 flex flex-col gap-2 px-2'>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/new-staff'}>New Staff</Link>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/staff-list'}>Staff List</Link>
         </div>
         }
       </div>
 
-      <div className='w-full flex flex-col gap-1'>
-        <button onClick={() => setOption('inventory')} className='cursor-pointer w-full text-left'>Inventory</button>
-        {option === 'inventory' && <div className='w-full border-l-2 flex flex-col gap-1 px-2'>
-          <Link href={'/panel/new-stock'}>Add Stock</Link>
-          <Link href={'/panel/stock-report'}>Stock Report</Link>
+      <div className='w-full flex flex-col gap-2 px-2'>
+        <button onClick={() => setOption('inventory')} className='cursor-pointer w-full text-left hover:bg-orange-300 hover:text-white flex flex-row items-center gap-4'><MdInventory/> Inventory</button>
+        {option === 'inventory' && <div className='w-full border-l-2 flex flex-col gap-2 px-2'>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/new-stock'}>Add Stock</Link>
+          <Link className='w-full hover:text-white hover:bg-orange-300 px-2' href={'/panel/stock-report'}>Stock Report</Link>
         </div>
         }
       </div>
 
-      <Link href={'/panel/customers'} onClick={() => setOption('')}>Customers</Link>
-      <Link href={'/panel/supports'} onClick={() => setOption('')}>Supports</Link>
+      <Link className='w-full hover:text-white hover:bg-orange-300 px-2 flex flex-row items-center gap-4' href={'/panel/customers'} onClick={() => setOption('')}><RiCustomSize/> Customers</Link>
+      <Link className='w-full hover:text-white hover:bg-orange-300 px-2 flex flex-row items-center gap-4' href={'/panel/supports'} onClick={() => setOption('')}> <RiCustomerServiceLine/>Supports</Link>
 
     </div>
   )
