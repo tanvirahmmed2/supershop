d17ef7e4f200ea184d2f7a9492b518a786ba72cc
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-03-02 23:10:16 +0600

    fetch

--- src/app/(main)/products/page.jsx
+++ src/app/(main)/products/page.jsx
@@ -1,9 +1,26 @@
 
 'use client'
-import React from 'react'
+import { Context } from '@/components/helper/Context'
+import React, { useContext, useEffect, useState } from 'react'
 
 const ProductsPage =  () => {
+
+  const {categories}= useContext(Context)
+  const [products, setProducts]= useState([])
+  const [page, setPage]=useState(1)
+  const [totalPage, setTotalPage]= useState(1)
+  const [category,setCategory]= useState('')
  
+  useEffect(()=>{
+    const fetchProducts=async()=>{
+      try {
+        
+      } catch (error) {
+        
+      }
+    }
+    fetchProducts()
+  },[])
 
   return (
     <div>
