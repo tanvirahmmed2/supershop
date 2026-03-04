09eb4b31d61621a54b15c5c630897579201a4893
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-03-04 15:56:09 +0600

    staff login

--- src/lib/database/secret.js
+++ src/lib/database/secret.js
@@ -1,5 +1,8 @@
 export const BASE_URL='http://localhost:3000'
 
+export const JWT_SECRET= process.env.JWT_SECRET
+export const NODE_ENV= process.env.NODE_ENV
+
 export const PG_PORT= process.env.PG_PORT
 export const PG_HOST=process.env.PG_HOST
 export const PG_DATABASE=process.env.PG_DATABASE
