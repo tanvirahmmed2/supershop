79c7862e8c26eafd68da3724632f1e245f620b49
Tanvir Ahmmed <tanvir004006@gmail.com>
2026-03-10 14:29:37 +0600

    user middleware

--- src/components/forms/UserLoginForm.jsx
+++ src/components/forms/UserLoginForm.jsx
@@ -1,11 +1,45 @@
 'use client'
-import React from 'react'
+import axios from 'axios'
+import Link from 'next/link'
+import React, { useState } from 'react'
+import { toast } from 'react-toastify'
 
 const UserLoginForm = () => {
-  return (
-    <div>
+  const [formData, setFormData]= useState({
+    email:'',
+    password:''
+  })
+
+  const handleChange=(e)=>{
+    const {name, value}= e.target
+    setFormData((prev)=>({...prev,[name]:value}))
+  }
+
+  const handleLogin=async(e)=>{
+    e.preventDefault()
+    try {
+      const res= await axios.post('/api/user/login', formData, {withCredentials:true})
+      toast.success(res.data.message)
+      window.location.replace('/profile')
+    } catch (error) {
+      console.log(error)
+      toast.error(error?.response?.data?.message || 'Failed to log in')
       
-    </div>
+    }
+  }
+  return (
+    <form onSubmit={handleLogin} className='w-full flex flex-col gap-4'>
+      <div className='flex flex-col gap-1 w-full'>
+        <label htmlFor="email">Email</label>
+        <input type="email" name='email' id='email' onChange={handleChange} value={formData.email} required  className='w-full px-3 p-1 border border-black/20 outline-none'/>
+      </div>
+      <div className='flex flex-col gap-1 w-full'>
+        <label htmlFor="password">Password</label>
+        <input type="password" id='password' name='password' value={formData.password} required onChange={handleChange} className='w-full px-3 p-1 border border-black/20 outline-none' />
+      </div>
+      <Link className='w-full text-right text-xs' href={'/staff-recover'}>Recover account</Link>
+      <button type='submit' className='px-4 p-1 bg-black text-white hover:bg-black/80 cursor-pointer'>Login</button>
+    </form>
   )
 }
 
---
+++ src/lib/usermiddleware.js
@@ -0,0 +1,34 @@
+import { cookies } from "next/headers"
+import jwt from 'jsonwebtoken'
+import { JWT_SECRET } from "./database/secret"
+import { pool } from "./database/db"
+
+
+
+export const isUserLogin = async () => {
+    try {
+        const token = (await cookies()).get('supershop_token')?.value
+        if (!token) {
+            return {
+                success: false, message: 'Please login'
+            }
+        }
+
+        const decode = jwt.verify(token, JWT_SECRET)
+
+        const data = await pool.query('SELECT user_id, email FROM users WHERE user_id=$1', [decode.id])
+        if (data.rowCount === 0) {
+            return { success: false, message: 'User not found' }
+        }
+        const user = data.rows[0]
+        return {
+            success: true, message: 'Login verified successfully', payload: user
+        }
+
+    } catch (error) {
+        return {
+            success: false, message: "Failed to verify login"
+        }
+
+    }
+}
